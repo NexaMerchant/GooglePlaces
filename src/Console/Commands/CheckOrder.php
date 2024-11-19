f0fddc0da446855dbfffef545bb15320d0ecf595
Steve <34465153+xxl4@users.noreply.github.com>
2024-11-19 14:16:42 +0800

    fix address check order

---
+++ src/Console/Commands/CheckOrder.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace NexaMerchant\GooglePlaces\Console\Commands;
+
+use Illuminate\Console\Command;
+use GuzzleHttp\Client;
+use Webkul\Sales\Repositories\OrderRepository;
+
+class CheckOrder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'GooglePlaces:check-order {--order_id=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check order address use Google Place Api';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(
+        OrderRepository $orderRepository
+    )
+    {
+        parent::__construct();
+        $this->orderRepository = $orderRepository;
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $this->info('Check order address use Google Place Api');
+
+        $order_id = $this->option('order_id');
+        $this->info('Order ID: ' . $order_id);
+
+        $order = $this->orderRepository->find($order_id);
+
+        $address = $order->shipping_address->address1;
+
+        //var_dump($address);exit;
+        $this->info('Address: ' . $address);
+
+
+        $client = new Client([
+            'base_uri' => 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+            'debug' => true,
+        ]);
+
+        $response = $client->request('GET', '', [
+            'query' => [
+               // 'input' => '1600 Amphitheatre Parkway, Mountain View, CA',
+                'input' => $address,
+                'inputtype' => 'textquery',
+                'fields' => 'formatted_address,name,geometry,plus_code,opening_hours,permanently_closed,photos,place_id,types',
+                'key' => config('GooglePlaces.google_place_api_key'),
+                'region' => $order->shipping_address->country
+            ],
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ]
+        ]);
+
+        var_dump($response->getBody()->getContents());
+
+
+    }
+}
