45946323c51d3a6e17c972183bdfa941ea462a16
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-09 10:41:02 +0800

    add order path

---
+++ src/Docs/V1/GooglePlaces/Controllers/Orders/OrdersController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace NexaMerchant\GooglePlaces\Docs\V1\GooglePlaces\Controllers\Orders;
+
+class OrdersController {
+
+    /**
+     * @OA\Get(
+     *      path="/orders",
+     *      operationId="getOrdersList",
+     *      tags={"Orders"},
+     *      summary="Get list of orders",
+     *      description="Returns list of orders",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Order")
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated"
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
+     */
+    public function getOrdersList() {
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/orders/{id}",
+     *      operationId="getOrderById",
+     *      tags={"Orders"},
+     *      summary="Get order information",
+     *      description="Returns order data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Order id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/Order")
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated"
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
+     */
+    public function getOrderById($id) {
+    }
+}
