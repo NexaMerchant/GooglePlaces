d12aa437efbd57ecea43f937842a27b879bcee27
Steve <34465153+xxl4@users.noreply.github.com>
2024-12-09 10:23:21 +0800

    fix add docs for api

---
+++ src/Docs/V1/GooglePlaces/Controllers/Controller.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace NexaMerchant\Apps\GooglePlaces\Docs\V1\GooglePlaces\Controllers;
+
+/**
+ * @OA\Info(
+ *      version="1.1.0",
+ *      title="NexaMerchant GooglePlaces Rest API Documentation",
+ *      description="NexaMerchant GooglePlaces Rest API Documentation",
+ *
+ *      @OA\Contact(
+ *          email="nice.lizhi@gmail.com",
+ *          name="NexaMerchant"
+ *     )
+ * )
+ * @OA\Server(
+ *     url=APP_URL,
+ *    description="NexaMerchant GooglePlaces End Rest API"
+ * )
+ * @OA\Tag(
+ *    name="GooglePlaces",
+ *   description="API Endpoints of GooglePlaces"
+ * )
+ */
+class Controller
+{
+}
